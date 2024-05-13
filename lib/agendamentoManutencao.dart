8c494f0275db106557a269387f1f62d9bf62047c
Andreyna Gonçalves Bastos <81197697+Andreyna618@users.noreply.github.com>
2024-05-13 16:02:12 -0300

    Update agendamentoManutencao.dart - scroll

--- lib/agendamentoManutencao.dart
+++ lib/agendamentoManutencao.dart
@@ -7,157 +7,156 @@ class AgendamentoManutencao extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "IETEL Solar",
-          style: ietelSolar,
+        appBar: AppBar(
+          title: const Text(
+            "IETEL Solar",
+            style: ietelSolar,
+          ),
+          iconTheme: const IconThemeData(color: Colors.white),
         ),
-        iconTheme: const IconThemeData(color: Colors.white),
-      ),
-      backgroundColor: const Color(0xFF082b59),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(
-                Radius.circular(6),
-              ),
-            ),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: const Color(0xFF082b59),
+        body: ListView(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(6),
+                    ),
+                  ),
+                  child: Column(
                     children: [
-                      Text(
-                        "Agendamento da ",
-                        style: titulo,
+                      const Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Agendamento da ",
+                              style: titulo,
+                            ),
+                            Text(
+                              "Manutenção",
+                              style: palavraChave,
+                            ),
+                            Text(
+                              ":",
+                              style: titulo,
+                            ),
+                          ],
+                        ),
                       ),
-                      Text(
-                        "Manutenção",
-                        style: palavraChave,
+
+                      //Data
+                      const Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15, top: 30),
+                        child: TextField(
+                          cursorColor: Color(0xFF082b59),
+                          cursorWidth: 1.5,
+                          style: TextStyle(fontSize: 14, color: Colors.black),
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: "Data",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xFF082b59),
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xFFF58934),
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                      Text(
-                        ":",
-                        style: titulo,
+
+                      //Horário
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          left: 15,
+                          right: 15,
+                          top: 15,
+                        ),
+                        child: TextField(
+                          cursorColor: Color(0xFF082b59),
+                          cursorWidth: 1.5,
+                          style: TextStyle(fontSize: 14, color: Colors.black),
+                          maxLength: 4,
+                          decoration: InputDecoration(
+                            counterText: '',
+                            hintText: "Horário",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xFF082b59),
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xFFF58934),
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                      ),
+
+                      //Botão
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 15, right: 15, bottom: 32, top: 30),
+                        child: SizedBox(
+                          width: 200,
+                          height: 36,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              overlayColor:
+                                  MaterialStateProperty.resolveWith<Color?>(
+                                (Set<MaterialState> states) {
+                                  if (states.contains(MaterialState.hovered))
+                                    return Color(0xFF082b59);
+                                  return null;
+                                },
+                              ),
+                            ),
+                            child: const Text(
+                              "AGENDAR",
+                              style: button,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-
-                //Data
-                const Padding(
-                  padding: EdgeInsets.only(
-                    left: 15,
-                    right: 15,
-                    top: 30
-                  ),
-                  child: TextField(
-                    cursorColor:  Color(0xFF082b59),
-                    cursorWidth: 1.5,
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: "Data",
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xFF082b59),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xFFF58934),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                //Horário
-                const Padding(
-                  padding: EdgeInsets.only(
-                    left: 15,
-                    right: 15,
-                    top: 15,
-                  ),
-                  child: TextField(
-                    cursorColor: Color(0xFF082b59),
-                    cursorWidth: 1.5,
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                    maxLength: 4,
-                    decoration: InputDecoration(
-                      counterText: '',
-                      hintText: "Horário",
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xFF082b59),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xFFF58934),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                    ),
-                    keyboardType: TextInputType.text,
-                  ),
-                ),
-
-
-                //Botão
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 15, right: 15, bottom: 32, top: 30),
-                  child: SizedBox(
-                    width: 200,
-                    height: 36,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                            if (states.contains(MaterialState.hovered))
-                              return Color(0xFF082b59);
-                            return null;
-                          },
-                        ),
-                      ),
-                      child: const Text(
-                        "AGENDAR",
-                        style: button,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          ],
+        ));
   }
 }
