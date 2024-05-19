d1c9ae5eca463907fc642d2ad0c5a3e52d803d7f
Gabrielly <gabriellyfernandaferreira1@gmail.com>
2024-05-19 16:48:23 -0300

    Banco de Dados.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:ietel_solar/app.dart';
 
+const firebaseConfig = FirebaseOptions(
+    apiKey: "AIzaSyDLJjwwFaY2J1K5shIN819A1gemgM3ezxQ",
+    authDomain: "userlist-413f7.firebaseapp.com",
+    projectId: "userlist-413f7",
+    storageBucket: "userlist-413f7.appspot.com",
+    messagingSenderId: "271234920509",
+    appId: "1:271234920509:web:bca90e85068cb6cb86fa54");
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: firebaseConfig);
   runApp(const App());
 }
